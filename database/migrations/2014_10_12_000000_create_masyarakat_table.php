c7aba4f70ed237351c090ddddbd79390384148b5
f3iru <this.fairuz@gmail.com>
2023-02-10 08:18:14 +0700

    first commit

---
+++ database/migrations/2014_10_12_000000_create_masyarakat_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('masyarakat', function (Blueprint $table) {
+            $table->char("nik", $length=16)->primary();
+            $table->string('nama');
+            $table->string('username')->unique();
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
