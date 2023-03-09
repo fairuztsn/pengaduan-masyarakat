5736cb5a1a5f4992a0cd38b860598040242ef3ed
f3iru <this.fairuz@gmail.com>
2023-03-09 09:27:22 +0700

    fix routeserviceprovider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,8 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/u';
-    public const ADMIN = "/a";
+    public const HOME = '/';
+    public const ADMIN = "/";
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
