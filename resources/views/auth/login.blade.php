49c0e729548ca25adeacecb327eefd03d596cbd3
f3iru <this.fairuz@gmail.com>
2023-03-20 12:35:55 +0700

    login view added alert if not-valid

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,11 +66,21 @@
             @csrf
             <div class="mb-3">
                 <label for="" class="form-label text-sm"><i class="fas fa-envelope me-3"></i>Email <span class="text-sm text-danger">*</span> </label>
-                <input type="email" name="email" id="email" class="form-control bg-white">
+                <input type="email" name="email" id="email" class="form-control bg-white @error('email') is-invalid @enderror">
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="" class="form-label text-sm"><i class="fas fa-key me-3"></i>Password <span class="text-sm text-danger">*</span> </label>
-                <input type="password" name="password" id="password" class="form-control bg-white">
+                <input type="password" name="password" id="password" class="form-control bg-white @error('password') is-invalid @enderror">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
             <div class="mb-3">
                 <button class="btn btn-outline-dark form-control mb-4" style="box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;"><i class="fas fa-right-to-bracket me-3"></i>Masuk</button>
