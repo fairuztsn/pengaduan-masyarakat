110fae489a3e54dfb25783f462d537775d84f53c
f3iru <this.fairuz@gmail.com>
2023-03-26 21:16:28 +0700

    ajax jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
 	{{-- jQuery --}}
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+	{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script> --}}
+	<script src="{{asset("/ajax/libs/jquery/3.6.3/jquery.min.js")}}"></script>
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
